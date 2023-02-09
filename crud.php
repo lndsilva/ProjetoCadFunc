a1dc71b6bf4ff3b6ab41659affd78a9e27ef2f02
lndsilva <lndsilva@hotmail.com>
2023-02-08 21:27:55 -0300

    inserindo comandos

---
+++ crud.php
@@ -0,0 +1,5 @@
+<?php 
+
+	echo "Conectando o crud";
+
+ ?>
--- index.php
+++ index.php
@@ -7,5 +7,19 @@
 </head>
 <body>
 
+	<h1>Gerenciar Funcionários</h1>
+
+	<form action="crud.php" method="post">
+		Nome: <input type="text" name="nome"> <br>
+		E-mail: <input type="email" name="email"><br>
+		CPF: <input type="text" name="cpf"><br>
+		Telefone: <input type="tel" name="telefone"><br>
+		<button>Cadastrar</button>
+		<button>Alterar</button>
+		<button>Excluir</button>
+		<button>Pesquisar</button>
+
+	</form>
+
 </body>
 </html>
