4cce48848945171b83f023846488bbb59599d7ac
lndsilva <lndsilva@hotmail.com>
2023-02-08 21:44:51 -0300

    css com bootstrap

--- index.php
+++ index.php
@@ -2,24 +2,66 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
 	<title>Cadastrar Funcionário</title>
 </head>
 <body>
 
-	<h1>Gerenciar Funcionários</h1>
+	 <div class="panel-heading">Gerenciar Funcionários</div>
 
-	<form action="crud.php" method="post">
-		Nome: <input type="text" name="nome"> <br>
-		E-mail: <input type="email" name="email"><br>
-		CPF: <input type="text" name="cpf"><br>
-		Telefone: <input type="tel" name="telefone"><br>
-		<button>Cadastrar</button>
-		<button>Alterar</button>
-		<button>Excluir</button>
-		<button>Pesquisar</button>
+	<div class="form-group">
+		<label class="col-md-2 control-label" for="Nome">Nome <h11>*</h11></label>
+	<div class="col-md-8">
+		<input id="Nome" name="Nome" placeholder="" class="form-control input-md" required="" type="text">
+	</div>
+</div>
+<div class="form-group">
+  <label class="col-md-2 control-label" for="Nome">CPF <h11>*</h11></label>  
+  <div class="col-md-2">
+  <input id="cpf" name="cpf" placeholder="Apenas números" class="form-control input-md" required="" type="text" maxlength="11" pattern="[0-9]+$">
+  </div>
 
-	</form>
+  <label class="col-md-1 control-label" for="prependedtext">Telefone</label>
+     <div class="col-md-2">
+    <div class="input-group">
+      <span class="input-group-addon"><i class="glyphicon glyphicon-earphone"></i></span>
+      <input id="prependedtext" name="prependedtext" class="form-control" placeholder="XX XXXXX-XXXX" type="text" maxlength="13"  pattern="\[0-9]{2}\ [0-9]{4,6}-[0-9]{3,4}$"
+      OnKeyPress="formatar('## #####-####', this)">
+    </div>
+  </div>
+ </div> 
+ <div class="form-group">
+  <label class="col-md-2 control-label" for="prependedtext">Email <h11>*</h11></label>
+  <div class="col-md-5">
+    <div class="input-group">
+      <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
+      <input id="prependedtext" name="prependedtext" class="form-control" placeholder="email@email.com" required="" type="text" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" >
+    </div>
+  </div>
+</div>
+
+ <div class="form-group">
+  <label class="col-md-2 control-label" for="Cadastrar"></label>
+  <div class="col-md-8">
+    <button id="Cadastrar" name="Cadastrar" class="btn btn-primary" type="Submit">Cadastrar</button>
+    <button id="Cadastrar" name="Cadastrar" class="btn btn-warning" type="Submit">Alterar</button>
+    <button id="Cadastrar" name="Cadastrar" class="btn btn-danger" type="Submit">Excluir</button>
+    <button id="Cadastrar" name="Cadastrar" class="btn btn-info" type="Submit">Pesquisar</button>
+
+    <button id="Cancelar" name="Cancelar" class="btn btn-danger" type="Reset">Cancelar</button>
+  </div>
+</div>
+
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+  
 
 </body>
 </html>
