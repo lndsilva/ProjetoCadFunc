24c2bd3302738df0f058315424f1abe864cc3ea3
lndsilva <lndsilva@hotmail.com>
2023-02-08 22:04:05 -0300

    alteração do código css

--- index.php
+++ index.php
@@ -10,55 +10,51 @@
 	<title>Cadastrar Funcionário</title>
 </head>
 <body>
+	<div  class="alert alert-primary" role="alert">Gerenciar Funcionários</div>
 
-	 <div  class="alert alert-primary" role="alert">Gerenciar Funcionários</div>
+		 <div class="container border border-primary bg-info">
+		 	<div class="form-group">
+		 		<label class="col-md-2 control-label" for="Nome">Nome <h11>*</h11></label>
+		 		<div class="col-md-8">
+		 			<input id="Nome" name="Nome" placeholder="" class="form-control input-md" required="" type="text">
+		 		</div>
+		 	</div>
+		   <div class="form-group">
+		   	<label class="col-md-2 control-label" for="Nome">CPF <h11>*</h11></label>
+		   	<div class="col-md-2">
+		   		<input id="cpf" name="cpf" placeholder="Apenas números" class="form-control input-md"
+		   		required="" type="text" maxlength="11" pattern="[0-9]+$">
+		   	</div>
 
-	 <div class="container border border-primary bg-info">
+		   	<label class="col-md-1 control-label" for="prependedtext">Telefone</label>
+		   	<div class="col-md-2">
+		   		<div class="input-group">
+		   			<span class="input-group-addon"><i class="glyphicon glyphicon-earphone"></i></span>
+		   			      <input id="prependedtext" name="prependedtext" class="form-control" placeholder="XX XXXXX-XXXX" type="text" maxlength="13"  pattern="\[0-9]{2}\ [0-9]{4,6}-[0-9]{3,4}$"OnKeyPress="formatar('## #####-####', this)">
+		   			  </div>
+		   			</div>
+		   		</div>
+		   		<div class="form-group">
+		   			<label class="col-md-2 control-label" for="prependedtext">Email <h11>*</h11></label>
+		   			<div class="col-md-5">
+		   				<div class="input-group">
+		   					<span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
+		   					<input id="prependedtext" name="prependedtext" class="form-control" placeholder="email@email.com" required="" type="text" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" >
+		   				</div>
+		   			</div>
+		   		</div>
+		   		<div class="form-group">
+		   			<label class="col-md-2 control-label" for="Cadastrar"></label>
+		   			<div class="col-md-8">
+		   				<button id="Cadastrar" name="Cadastrar" class="btn btn-primary" type="Submit">Cadastrar</button>
+		   				<button id="Cadastrar" name="Cadastrar" class="btn btn-warning" type="Submit">Alterar</button>
+		   				<button id="Cadastrar" name="Cadastrar" class="btn btn-danger" type="Submit">Excluir</button>
+		   				<button id="Cadastrar" name="Cadastrar" class="btn btn-info" type="Submit">Pesquisar</button>
+		   				<button id="Cancelar" name="Cancelar" class="btn btn-danger" type="Reset">Cancelar</button>
+		   			</div>
+		   		</div>
 
-	<div class="form-group">
-		<label class="col-md-2 control-label" for="Nome">Nome <h11>*</h11></label>
-	<div class="col-md-8">
-		<input id="Nome" name="Nome" placeholder="" class="form-control input-md" required="" type="text">
-	</div>
-</div>
-<div class="form-group">
-  <label class="col-md-2 control-label" for="Nome">CPF <h11>*</h11></label>  
-  <div class="col-md-2">
-  <input id="cpf" name="cpf" placeholder="Apenas números" class="form-control input-md" required="" type="text" maxlength="11" pattern="[0-9]+$">
-  </div>
-
-  <label class="col-md-1 control-label" for="prependedtext">Telefone</label>
-     <div class="col-md-2">
-    <div class="input-group">
-      <span class="input-group-addon"><i class="glyphicon glyphicon-earphone"></i></span>
-      <input id="prependedtext" name="prependedtext" class="form-control" placeholder="XX XXXXX-XXXX" type="text" maxlength="13"  pattern="\[0-9]{2}\ [0-9]{4,6}-[0-9]{3,4}$"
-      OnKeyPress="formatar('## #####-####', this)">
-    </div>
-  </div>
- </div> 
- <div class="form-group">
-  <label class="col-md-2 control-label" for="prependedtext">Email <h11>*</h11></label>
-  <div class="col-md-5">
-    <div class="input-group">
-      <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
-      <input id="prependedtext" name="prependedtext" class="form-control" placeholder="email@email.com" required="" type="text" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" >
-    </div>
-  </div>
-</div>
-
- <div class="form-group">
-  <label class="col-md-2 control-label" for="Cadastrar"></label>
-  <div class="col-md-8">
-    <button id="Cadastrar" name="Cadastrar" class="btn btn-primary" type="Submit">Cadastrar</button>
-    <button id="Cadastrar" name="Cadastrar" class="btn btn-warning" type="Submit">Alterar</button>
-    <button id="Cadastrar" name="Cadastrar" class="btn btn-danger" type="Submit">Excluir</button>
-    <button id="Cadastrar" name="Cadastrar" class="btn btn-info" type="Submit">Pesquisar</button>
-
-    <button id="Cancelar" name="Cancelar" class="btn btn-danger" type="Reset">Cancelar</button>
-  </div>
-</div>
-
-</div>
+		   	</div>
 
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
